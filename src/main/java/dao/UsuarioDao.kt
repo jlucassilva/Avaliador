91b3fb9c551f61a3e14ffb5f69c6b559315cdf6a
joao.lucas <joao.lucas@plannersistemas.com.br>
2018-11-12 17:57:36 -0300

    update
    Adicionando banco de dados em memoria e ExtensionFunction

--- src/main/java/dao/UsuarioDao.kt
+++ src/main/java/dao/UsuarioDao.kt
@@ -2,18 +2,25 @@ package dao
 
  import model.Usuario
  import java.io.Serializable
+ import javax.persistence.NoResultException
+ import javax.persistence.TypedQuery
 
 open class UsuarioDao: AbstractDao<Usuario>(Usuario::class.java),Serializable {
-
-    fun findUserWith(username: String, password: String): Usuario? {
+    
+    fun findUserWith(username: String, password: String): Usuario {
         return getEntityManager().createQuery("select u from Usuario u" +
                 " where u.username = :username" +
                 " and   u.password = :password", Usuario::class.java)
                 .setParameter("username", username)
                 .setParameter("password", password)
-                .resultList
-                .stream()
-                .findFirst()
-                .orElse(null)
+                .safeSingleResult()
     }
-}
+
+   fun TypedQuery<Usuario>.safeSingleResult(): model.Usuario {
+        return try {
+            this.singleResult
+        } catch (e: NoResultException) {
+            e.printStackTrace()
+            Usuario()
+        }
+    }}
